f9a09ecd84b6080c0eeacc17e3a1c768a2416605
tjrelly <tjrelly@TJs-Laptop>
2024-06-15 14:18:48 -0400

    step 1 complete

---
+++ step1.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+function cat(path) {
+  try {
+    // store the read file contents
+    var contents = fs.readFileSync(path, "utf8");
+    console.log(`file contents are "${contents}"`);
+  } catch (error) {
+    // errors thrown by fs will be caught here
+    console.error(error);
+    // kill the process and tell the shell it errored
+    process.exit(1);
+  }
+}
+
+cat("one.txt")
